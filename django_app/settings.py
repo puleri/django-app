3d96b5012eba6949297ad39a3ef0a5faa49e98d8
mp4 <puleri.mp4@gmail.com>
2020-12-21 07:05:16 -0800

    Deployment fixes

--- django_app/settings.py
+++ django_app/settings.py
@@ -38,7 +38,9 @@ else:
   # Set debug to false
   DEBUG = False
   # Only allow the `CLIENT_ORIGIN` for CORS
-  CORS_ORIGIN_WHITELIST = ['https://puleri.github.io/django-app-react/']
+  CORS_ORIGIN_WHITELIST = [
+    os.getenv('CLIENT_ORIGIN')
+  ]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
